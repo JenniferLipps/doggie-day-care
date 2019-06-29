9d2d0602d7c3d3109937aff3a72d78f6c7e5f354
Jennifer Lipps <jjlipps@gmail.com>
2019-06-29 12:05:34 -0500

    wip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,19 +4,30 @@ import DogPen from '../DogPen/DogPen';
 import StaffRoom from '../StaffRoom/StaffRoom';
 
 import './Home.scss';
+import WalkSchedule from '../WalkSchedule/WalkSchedule';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
+        <div>
+          <h2>Schedule a Walk</h2>
+          <form>
+            <input placeholder="Date"></input>
+          </form>
+        </div>
         <div className="row">
-          <div className="col">
+          <div className="DogPen card-group col">
             <h2>The Dogs</h2>
             <DogPen />
           </div>
-          <div className="col">
+          <div className="StaffRoom col">
             <h2>The People</h2>
             <StaffRoom />
+          </div>
+          <div className="WalkSchedule col">
+            <h2>Scheduled Walks</h2>
+            <WalkSchedule />
           </div>
         </div>
       </div>
